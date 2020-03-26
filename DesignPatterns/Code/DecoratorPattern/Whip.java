8d9a2cf51ad462a7095ad0f9f99e46ddc13d295c
Andrew <hqpan@foxmail.com>
2020-03-26 23:16:47 +0800

    Add page 259 of design pattern.

---
+++ DesignPatterns/Code/DecoratorPattern/Whip.java
@@ -0,0 +1,16 @@
+package DecoratorPattern;
+
+public class Whip extends CondimentDecorator {
+    Beverage beverage;
+    public Whip(Beverage beverage) {
+        this.beverage = beverage;
+    }
+
+    public String getDescription() {
+        return beverage.getDescription() + ", Whip";
+    }
+
+    public double cost() {
+        return beverage.cost() + 0.5;
+    }
+}
